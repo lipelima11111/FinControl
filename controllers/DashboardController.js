36a298bece76709c4935e59225f9bdaa79355660
Felipe Lima Duarte <felipe.duarte@conab.gov.br>
2026-05-28 19:37:13 -0300

    feat: implementa dashboard com saldo e histórico

---
+++ controllers/DashboardController.js
@@ -0,0 +1,34 @@
+const DashboardService = require('../services/DashboardService');
+const viewHelper = require('../utils/viewHelper');
+
+class DashboardController {
+  async index(req, res) {
+    try {
+      const usuarioId = req.session.usuarioId;
+      const { saldo, receitas, despesas } = await DashboardService.calcularSaldoTotal(usuarioId);
+      const historico = await DashboardService.buscarHistorico(usuarioId);
+      const historicoView = viewHelper.mapHistorico(historico);
+
+      res.render('dashboard/index', {
+        usuarioNome: req.session.usuarioNome,
+        saldoFormatado: viewHelper.formatMoeda(saldo),
+        receitasFormatado: viewHelper.formatMoeda(receitas),
+        despesasFormatado: viewHelper.formatMoeda(despesas),
+        historico: historicoView,
+        historicoVazio: historicoView.length === 0,
+      });
+    } catch (e) {
+      res.render('dashboard/index', {
+        usuarioNome: req.session.usuarioNome,
+        saldoFormatado: viewHelper.formatMoeda(0),
+        receitasFormatado: viewHelper.formatMoeda(0),
+        despesasFormatado: viewHelper.formatMoeda(0),
+        historico: [],
+        historicoVazio: true,
+        erro: e.message,
+      });
+    }
+  }
+}
+
+module.exports = new DashboardController();
